9517bd9499257c4d02fb9b4022e4143bb38e4990
huwenkang <528860398@qq.com>
2023-02-03 23:02:32 +0800

    add page function of DishController

--- src/main/java/com/huwenkang/reggie/controller/DishController.java
+++ src/main/java/com/huwenkang/reggie/controller/DishController.java
@@ -1,28 +1,77 @@
 package com.huwenkang.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.huwenkang.reggie.common.R;
 import com.huwenkang.reggie.dto.DishDto;
+import com.huwenkang.reggie.entity.Category;
+import com.huwenkang.reggie.entity.Dish;
+import com.huwenkang.reggie.service.CategoryService;
 import com.huwenkang.reggie.service.DishFlavorService;
 import com.huwenkang.reggie.service.DishService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/dish")
 public class DishController {
     @Autowired
-    DishService dishService;
+    private DishService dishService;
 
     @Autowired
-    DishFlavorService dishFlavorService;
+    private DishFlavorService dishFlavorService;
+
+    @Autowired
+    private CategoryService categoryService;
 
     @PostMapping
-    public R<String> save(@RequestBody DishDto dishDto){
+    public R<String> save(@RequestBody DishDto dishDto) {
         dishService.saveWithFlavor(dishDto);
 
         return R.success("新增菜品成功");
     }
+
+    @GetMapping("/page")
+    public R<Page<DishDto>> page(int page, int pageSize, String name) {
+        Page<Dish> dishPage = new Page<>(page, pageSize);
+
+        Page<DishDto> dishDtoPage = new Page<>();
+
+        LambdaQueryWrapper<Dish> dishLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        dishLambdaQueryWrapper.like(name != null, Dish::getName, name);
+
+        dishLambdaQueryWrapper.orderByDesc(Dish::getUpdateTime);
+
+        dishService.page(dishPage, dishLambdaQueryWrapper);
+        BeanUtils.copyProperties(dishPage, dishDtoPage, "records");
+
+        List<Dish> records = dishPage.getRecords();
+
+        List<DishDto> dishDtoList = records.stream().map((item) -> {
+            Long categoryId = item.getCategoryId();
+
+            Category category = categoryService.getById(categoryId);
+
+
+            DishDto dishDto = new DishDto();
+            Optional<Category> optional = Optional.ofNullable(category);
+            optional.ifPresent(category1 -> dishDto.setCategoryName(category1.getName()));
+
+            BeanUtils.copyProperties(item, dishDto);
+
+
+            return dishDto;
+        }).collect(Collectors.toList());
+
+        dishDtoPage.setRecords(dishDtoList);
+
+        return R.success(dishDtoPage);
+    }
+
 }
